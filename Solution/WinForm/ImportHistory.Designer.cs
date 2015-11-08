ae258d37f8c0ae0e8379949a9bb8b58b18e5700a
Jesse <weihongji@gmail.com>
2015-11-08 11:24:28 +0800

    Refined Import History form.

--- Solution/WinForm/ImportHistory.Designer.cs
+++ Solution/WinForm/ImportHistory.Designer.cs
@@ -25,32 +25,23 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.panelDataGrid = new System.Windows.Forms.Panel();
 			this.panelCommand = new System.Windows.Forms.Panel();
 			this.btnRefresh = new System.Windows.Forms.Button();
 			this.lblTitle = new System.Windows.Forms.Label();
 			this.btnClose = new System.Windows.Forms.Button();
+			this.panelData = new System.Windows.Forms.Panel();
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.panelDataGrid.SuspendLayout();
 			this.panelCommand.SuspendLayout();
+			this.panelData.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// panelDataGrid
-			// 
-			this.panelDataGrid.Controls.Add(this.dataGridView1);
-			this.panelDataGrid.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.panelDataGrid.Location = new System.Drawing.Point(0, 61);
-			this.panelDataGrid.Name = "panelDataGrid";
-			this.panelDataGrid.Size = new System.Drawing.Size(684, 501);
-			this.panelDataGrid.TabIndex = 0;
 			// 
 			// panelCommand
 			// 
 			this.panelCommand.Controls.Add(this.btnRefresh);
 			this.panelCommand.Controls.Add(this.lblTitle);
 			this.panelCommand.Controls.Add(this.btnClose);
-			this.panelCommand.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelCommand.Dock = System.Windows.Forms.DockStyle.Top;
 			this.panelCommand.Location = new System.Drawing.Point(0, 0);
 			this.panelCommand.Name = "panelCommand";
 			this.panelCommand.Size = new System.Drawing.Size(684, 61);
@@ -62,7 +53,7 @@
 			this.btnRefresh.Location = new System.Drawing.Point(439, 17);
 			this.btnRefresh.Name = "btnRefresh";
 			this.btnRefresh.Size = new System.Drawing.Size(91, 26);
-			this.btnRefresh.TabIndex = 2;
+			this.btnRefresh.TabIndex = 1;
 			this.btnRefresh.Text = "刷新 (&R)";
 			this.btnRefresh.UseVisualStyleBackColor = true;
 			this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
@@ -74,7 +65,7 @@
 			this.lblTitle.Location = new System.Drawing.Point(203, 20);
 			this.lblTitle.Name = "lblTitle";
 			this.lblTitle.Size = new System.Drawing.Size(138, 21);
-			this.lblTitle.TabIndex = 1;
+			this.lblTitle.TabIndex = 0;
 			this.lblTitle.Text = "数据导入记录查询";
 			// 
 			// btnClose
@@ -84,20 +75,26 @@
 			this.btnClose.Location = new System.Drawing.Point(564, 17);
 			this.btnClose.Name = "btnClose";
 			this.btnClose.Size = new System.Drawing.Size(91, 26);
-			this.btnClose.TabIndex = 0;
+			this.btnClose.TabIndex = 2;
 			this.btnClose.Text = "关闭 (&C)";
 			this.btnClose.UseVisualStyleBackColor = true;
 			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
 			// 
+			// panelData
+			// 
+			this.panelData.Controls.Add(this.dataGridView1);
+			this.panelData.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelData.Location = new System.Drawing.Point(0, 61);
+			this.panelData.Name = "panelData";
+			this.panelData.Size = new System.Drawing.Size(684, 501);
+			this.panelData.TabIndex = 2;
+			// 
 			// dataGridView1
 			// 
-			this.dataGridView1.AllowUserToAddRows = false;
-			this.dataGridView1.AllowUserToDeleteRows = false;
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.dataGridView1.Location = new System.Drawing.Point(0, 0);
 			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.ReadOnly = true;
 			this.dataGridView1.RowTemplate.Height = 23;
 			this.dataGridView1.Size = new System.Drawing.Size(684, 501);
 			this.dataGridView1.TabIndex = 0;
@@ -108,15 +105,16 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.btnClose;
 			this.ClientSize = new System.Drawing.Size(684, 562);
+			this.Controls.Add(this.panelData);
 			this.Controls.Add(this.panelCommand);
-			this.Controls.Add(this.panelDataGrid);
+			this.MinimumSize = new System.Drawing.Size(700, 400);
 			this.Name = "frmImportHistory";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "数据导入查询";
 			this.Load += new System.EventHandler(this.frmImportHistory_Load);
-			this.panelDataGrid.ResumeLayout(false);
 			this.panelCommand.ResumeLayout(false);
 			this.panelCommand.PerformLayout();
+			this.panelData.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
 
@@ -124,11 +122,11 @@
 
 		#endregion
 
-		private System.Windows.Forms.Panel panelDataGrid;
 		private System.Windows.Forms.Panel panelCommand;
 		private System.Windows.Forms.Label lblTitle;
 		private System.Windows.Forms.Button btnClose;
 		private System.Windows.Forms.Button btnRefresh;
+		private System.Windows.Forms.Panel panelData;
 		private System.Windows.Forms.DataGridView dataGridView1;
 	}
 }
