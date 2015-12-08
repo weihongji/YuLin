1e54f248fe07b565a961b6ef7038d71cfe69c377
Jesse <weihongji@gmail.com>
2015-12-08 22:10:02 +0800

    Add StartSqlServer() function

--- Deploy/Deployer/Deployer/FormMain.cs
+++ Deploy/Deployer/Deployer/FormMain.cs
@@ -20,6 +20,7 @@ namespace Deployer
 		}
 
 		private void FormMain_Load(object sender, EventArgs e) {
+			StartSqlServer();
 			this.lblVersionText.Text = ConfigurationManager.AppSettings["Version"];
 			this.lblDateText.Text = ConfigurationManager.AppSettings["ReleaseDate"];
 		}
@@ -100,6 +101,56 @@ namespace Deployer
 			sql.AppendLine("	EXEC sp_detach_db 'YuLin'");
 			sql.AppendLine("END");
 			dao.ExecuteNonQuery(sql.ToString());
+		}
+
+		// Copied from the YuLin project.
+		private int StartSqlServer() {
+			//logger.Debug("Checking sql server service...");
+			int result = 0;
+			Process[] sqlservers = Process.GetProcessesByName("sqlservr");
+			if (sqlservers.Length == 0) {
+				try {
+					var sqlinstance = GetSqlServerInstance();
+					Process process = new Process();
+					process.StartInfo.FileName = "net";
+					process.StartInfo.Arguments = "start " + sqlinstance;
+					process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+					process.Start();
+					process.WaitForExit();
+					process.Close();
+					sqlservers = Process.GetProcessesByName("sqlservr");
+					if (sqlservers.Length > 0) {
+						result = 1;
+						//logger.Info("Sql server started");
+					}
+					else {
+						result = 2;
+						//logger.Info("Sql server failed to start");
+					}
+				}
+				catch {
+					//logger.Error("Failed to start sql server.\r\n", ex);
+					result = 2;
+				}
+			}
+			//logger.Debug("Starting service result: " + result.ToString());
+			return result;
+		}
+
+		private string GetSqlServerInstance() {
+			var result = "MSSQLSERVER";
+			try {
+				var cnnStr = ConfigurationManager.ConnectionStrings["master"].ConnectionString;
+				var cnn = new System.Data.SqlClient.SqlConnection(cnnStr);
+				var server = cnn.DataSource;
+				if (server.IndexOf("\\") > 0) {
+					result = string.Format("MSSQL${0}", server.Substring(server.IndexOf("\\") + 1));
+				}
+			}
+			catch {
+				//logger.Error("Failed when getting sql server instance name:\r\n", ex);
+			}
+			return result;
 		}
 	}
 }
--- Solution/WinForm/Main.cs
+++ Solution/WinForm/Main.cs
@@ -64,6 +64,8 @@ namespace Reporting
 		}
 
 		private void Main_Load(object sender, EventArgs e) {
+			StartSqlServer();
+
 			this.calendarImport.Left = 206;
 			this.calendarImport.Visible = false;
 			this.btnSelectColumns.Visible = false;
@@ -841,6 +843,66 @@ namespace Reporting
 
 		public static void ShowErrorDialog(string msg, string title) {
 			MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		/// <summary>
+		/// Try to start SQL Server service if it is not running.
+		/// </summary>
+		/// <remarks>
+		/// This is used to handle the issue that SQL Server service stops for unknown reason.
+		/// </remarks>
+		/// <returns>
+		/// 0: Already running, nothing to do
+		/// 1: Success to start
+		/// 2: Failed to start
+		/// </returns>
+		private int StartSqlServer() {
+			logger.Debug("Checking sql server service...");
+			int result = 0;
+			Process[] sqlservers = Process.GetProcessesByName("sqlservr");
+			if (sqlservers.Length == 0) {
+				try {
+					var sqlinstance = GetSqlServerInstance();
+					Process process = new Process();
+					process.StartInfo.FileName = "net";
+					process.StartInfo.Arguments = "start " + sqlinstance;
+					process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+					process.Start();
+					process.WaitForExit();
+					process.Close();
+					sqlservers = Process.GetProcessesByName("sqlservr");
+					if (sqlservers.Length > 0) {
+						result = 1;
+						logger.Info("Sql server started");
+					}
+					else {
+						result = 2;
+						logger.Info("Sql server failed to start");
+					}
+				}
+				catch (Exception ex) {
+					logger.Error("Failed to start sql server.\r\n", ex);
+					result = 2;
+				}
+			}
+			logger.Debug("Starting service result: " + result.ToString());
+			return result;
+		}
+
+		private string GetSqlServerInstance() {
+			var result = "MSSQLSERVER";
+			try {
+				var cnnStr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+				var cnn = new System.Data.SqlClient.SqlConnection(cnnStr);
+				var server = cnn.DataSource;
+				if (server.IndexOf("\\") > 0) {
+					result = string.Format("MSSQL${0}", server.Substring(server.IndexOf("\\") + 1));
+				}
+			}
+			catch (Exception ex) {
+				logger.Error("Failed when getting sql server instance name:\r\n", ex);
+			}
+			return result;
 		}
 		#endregion
 
